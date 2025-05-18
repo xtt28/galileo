60a4c9dc81a265a69435e4a771416a605492e563
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-05-18 19:14:26 -0400

    Create basic user interface.

--- app/app.go
+++ app/app.go
@@ -1,9 +1,17 @@
 package app
 
 import (
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/xtt28/galileo/config"
+	"github.com/xtt28/galileo/ui"
+
+	"log"
 )
 
 func Run() {
 	config.ReadConfig()
+	prog := tea.NewProgram(ui.InitialModel())
+	if _, err := prog.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
