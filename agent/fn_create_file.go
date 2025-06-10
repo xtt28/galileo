9a44137acbc2ebf0c60cccaf69247982a9108020
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-06-10 12:23:14 -0400

    Whoopsies. :(

--- agent/fn_create_file.go
+++ agent/fn_create_file.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"github.com/openai/openai-go"
@@ -13,7 +14,7 @@ func createCreateFileFunc() AgentFunction {
 	invoke := func(w fyne.Window, call openai.ChatCompletionMessageToolCall) openai.ChatCompletionMessageParamUnion {
 		var args map[string]any
 		json.Unmarshal([]byte(call.Function.Arguments), &args)
-		path := args["path"].(string)
+		path := filepath.Join(getUserDocumentsPath(), args["path"].(string))
 		file, err := os.Create(path)
 		defer file.Close()
 		if err != nil {
@@ -35,7 +36,7 @@ func createCreateFileFunc() AgentFunction {
 	param := openai.FunctionDefinitionParam{
 		Name: "create_file",
 		Strict: openai.Bool(true),
-		Description: openai.String("Creates a file in the given path with the given content. Asks the user for permission first."),
+		Description: openai.String("Creates a file in the given path with the given content. The path is relative to the user's documents folder."),
 		Parameters: openai.FunctionParameters{
 			"type": "object",
 			"properties": map[string]any{
